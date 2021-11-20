ce06a0226e96d71650d4ccbb8ee11b751246b6b6
justin <28554768@qq.com>
2021-11-21 00:08:01 +0800

    添加sql

--- sql/commom/common.sql
+++ sql/commom/common.sql
@@ -43,4 +43,17 @@ create unique index uidx_code on setting_info
    setting_code
 );
 
+create table third_bind
+(
+   third_bind_id        bigint not null auto_increment comment '配置主键',
+   third_bind_name      varchar(256) comment '配置名称',
+   third_bind_params    text comment '配置值(json)',
+   create_time          datetime comment '创建时间',
+   del_status           int comment '删除状态(1、正常,2、删除)',
+   third_bind_code      varchar(128) comment '配置编码',
+   primary key (third_bind_id)
+);
+
+alter table third_bind comment '第三方绑定信息表';
+
 
