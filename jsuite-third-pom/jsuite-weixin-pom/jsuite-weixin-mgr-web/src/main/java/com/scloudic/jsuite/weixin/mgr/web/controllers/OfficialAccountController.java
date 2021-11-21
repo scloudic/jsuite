28fc2a5ec6d25ca6f968ba91699eadda1cddcd1c
liangjy <28554768@qq.com>
2021-11-21 20:49:31 +0800

    添加微信公众号接口

--- jsuite-third-pom/jsuite-weixin-pom/jsuite-weixin-mgr-web/src/main/java/com/scloudic/jsuite/weixin/mgr/web/controllers/OfficialAccountController.java
+++ jsuite-third-pom/jsuite-weixin-pom/jsuite-weixin-mgr-web/src/main/java/com/scloudic/jsuite/weixin/mgr/web/controllers/OfficialAccountController.java
@@ -1,17 +1,36 @@
 package com.scloudic.jsuite.weixin.mgr.web.controllers;
 
 import com.scloudic.rabbitframework.web.AbstractContextResource;
+import com.scloudic.rabbitframework.web.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Singleton;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 /**
  * 微信公众号
  */
 @Component
-@Path("/jsuite/officialAccount")
+@Path("/jsuite/weixin/officialAccount")
 @Singleton
 public class OfficialAccountController extends AbstractContextResource {
+    private static final Logger logger = LoggerFactory.getLogger(OfficialAccountController.class);
 
+    @GET
+    @Path("notify")
+    @Produces(MediaType.TEXT_HTML)
+    public String notify(@QueryParam("signature") String signature,
+                         @QueryParam("timestamp") String timestamp,
+                         @QueryParam("nonce") String nonce,
+                         @QueryParam("echostr") String echostr) {
+        logger.debug("signature:" + signature + ",timestamp:" + timestamp + ",nonce:" + nonce + ",echostr:" + echostr);
+        return echostr;
+
+    }
 }
