06c2799aa215f8ded7001aa73ca0162979550519
liangjy <28554768@qq.com>
2022-06-07 20:57:19 +0800

    优化

--- jsuite-third-pom/jsuite-weixin-pom/jsuite-weixin-service/src/main/java/com/scloudic/jsuite/weixin/mapper/WeiXinMpMessageMapper.java
+++
@@ -1,9 +0,0 @@
-package com.scloudic.jsuite.weixin.mapper;
-import com.scloudic.jsuite.weixin.entity.WeiXinMpMessage;
-import com.scloudic.rabbitframework.jbatis.mapping.BaseMapper;
-import com.scloudic.rabbitframework.jbatis.annontations.Mapper;
-
-@Mapper
-public interface WeiXinMpMessageMapper extends BaseMapper<WeiXinMpMessage> {
-
-}
--- jsuite-third-pom/jsuite-weixin-pom/jsuite-weixin-service/src/main/java/com/scloudic/jsuite/weixin/service/WeiXinMpMessageService.java
+++
@@ -1,8 +0,0 @@
-package com.scloudic.jsuite.weixin.service;
-import com.scloudic.jsuite.weixin.entity.WeiXinMpMessage;
-import com.scloudic.rabbitframework.jbatis.service.IService;
-
-public interface WeiXinMpMessageService extends IService<WeiXinMpMessage> {
-
-}
-
